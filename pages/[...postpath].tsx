e561025357632497bd19e15d06fd7b8bc42b5c46
Shahid Ali <132940375+KINGGGGGA@users.noreply.github.com>
2024-12-02 11:23:36 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`wwww.facebook.com`
+					`www.google.com`
 				}`,
 			},
 		};
