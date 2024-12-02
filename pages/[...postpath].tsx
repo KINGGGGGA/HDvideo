7f77481262c2a56ba628e043e7736bf61deb2741
Shahid Ali <132940375+KINGGGGGA@users.noreply.github.com>
2024-12-02 11:17:49 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://blog.wrgames.online/top-hospitals-in-chennai/?utm_source=PK&utm_medium=SS_Pro&utm_campaign=SS_Pro`
+					`wwww.facebook.com`
 				}`,
 			},
 		};
